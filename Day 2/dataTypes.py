3a0dbac667fcb7f832fd5b9838f8a589727895bd
SouvikSaha449 <souviksaha412@gmail.com>
2024-03-17 14:35:50 +0530

    Learned In Day 2

---
+++ Day 2/dataTypes.py
@@ -0,0 +1,5 @@
+print("Hello"[4])
+
+char = 1236
+
+print(type(char))
---
+++ Day 2/fstrings.py
@@ -0,0 +1,8 @@
+# f-strings are string literals that have an f before the opening quotation mark. They can include Python expressions enclosed in curly braces. Python will replace those expressions with their resulting values.
+
+
+score = 8
+height = 1.67
+isWinning = True
+
+print(f"Your score is {score} and height is {height} and you are winning {isWinning}")
