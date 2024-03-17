8504d8eb20637a9347dd32cc0a01bf5d25896a33
SouvikSaha449 <souviksaha412@gmail.com>
2024-03-17 14:39:28 +0530

    All about loops in Python

---
+++ Day 5/loops.py
@@ -0,0 +1,14 @@
+fruits = ["Pears","Apples", "Grapes"]
+for fruit in fruits:
+    print(fruit)
+
+
+#for loops in range
+
+for i in range(1,11,2):
+    print(i)
+
+sum=0
+for number in range(1,101):
+    sum+=number
+print(f"The sum of numbers are {sum}")
