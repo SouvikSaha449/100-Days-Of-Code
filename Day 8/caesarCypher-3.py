27fd5f1fd7a7b525bc823903c046da2297155aaa
SouvikSaha449 <souviksaha412@gmail.com>
2024-03-20 16:29:18 +0530

    All the steps of caesar cypher

---
+++ Day 8/caesarCypher-3.py
@@ -0,0 +1,25 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+#TODO-1: Combine the encrypt() and decrypt() functions into a single function called caesar().
+
+def caesar(plaintext, shiftno, direction):
+    end_text = ""
+
+    if direction == "decode":    
+        shiftno*=-1
+
+    for char in plaintext:
+        position=alphabet.index(char)
+        new_position = position+shiftno
+        end_text+=alphabet[new_position]
+    print(f"The {direction}d text is {end_text}")
+                    
+
+#TODO-2: Call the caesar() function, passing over the 'text', 'shift' and 'direction' values.
+
+caesar(plaintext=text, shiftno=shift, direction=direction)
+
